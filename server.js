67e1aee5da3437f461e8fdb16deebaa9f5f25534
Adewambe Daniel <danoscolobo@gmail.com>
2022-11-03 10:48:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var server = express();
+var cors = require("cors");
+require("dotenv").config();
+var bodyParser = require("body-parser");
+const EnumType = {
+  addition: "+",
+  substraction: "-",
+  multiplication: "*",
+};
+
+server.use(cors());
+
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+server.use(jsonParser);
+server.use(urlencodedParser);
+
+server.post("/details", (req, res) => {
+  const { operation_type, x, y } = req.body;
+
+  if (!EnumType[operation_type]) {
+    res.status(405).json({
+      success: false,
+      msg: "method not allowed",
+    });
+  } else {
+    result = 0;
+    if (operation_type == "addition") {
+      result = x + y;
+    } else if (operation_type == "multiplication") {
+      result = x * y;
+    } else {
+      result = x - y;
+    }
+    const data = {
+      slackUsername: "dannyx",
+      operation_type: EnumType.operation_type,
+      result: result,
+    };
+
+    res.status(200).json(data);
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}`);
+});
